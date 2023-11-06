626c4eaf50e152fa13eb18393a64785f01f2913d
annacwu <annacwu04@gmail.com>
2023-11-06 15:55:35 -0800

    Initial Commit

---
+++ Weatherful/WeatherfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherfulApp.swift
+//  Weatherful
+//
+//  Created by Anna Wu on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
