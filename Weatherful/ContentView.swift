afc7d5dcd8f7a4295d53432dd9b089bc0b08dd3e
annacwu <annacwu04@gmail.com>
2023-11-13 20:45:16 -0800

    outlined codable structs for api call

--- Weatherful/ContentView.swift
+++ Weatherful/ContentView.swift
@@ -7,6 +7,22 @@
 
 import SwiftUI
 
+func performAPICall() async throws {
+    // need lat long
+    // json wrapper?
+    // user id
+}
+
+struct Response: Codable{
+    var result: [Result]
+}
+
+struct Result: Codable {
+    var id = UUID()
+    var username: String
+    var name: String
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
